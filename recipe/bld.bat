0bd2417b2b9828c0ec16f29dee9d4d2114299036
Isuru Fernando <isuruf@gmail.com>
2020-01-17 12:59:34 -0600

    mingw->mingw32

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,1 @@
-python setup.py build_ext --compiler=mingw --inplace install --single-version-externally-managed --record record.txt
+python setup.py build_ext --compiler=mingw32 --inplace install --single-version-externally-managed --record record.txt

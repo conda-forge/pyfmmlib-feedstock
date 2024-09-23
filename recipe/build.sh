08ce541af4fa98cab6d6a05e83c7ecefea5bb51e
Matthias Diener <mdiener@illinois.edu>
2024-09-23 17:29:33 -0500

    cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,21 +6,13 @@ mkdir builddir
 
 export LDFLAGS="$LDFLAGS -fopenmp"
 
-# Taken from scipy-feedstock
+# {{{ Taken from https://github.com/conda-forge/numpy-feedstock/blob/main/recipe/build.sh
+
 # HACK: extend $CONDA_PREFIX/meson_cross_file that's created in
 # https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
 # https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
 # to use host python; requires that [binaries] section is last in meson_cross_file
 echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
-
-# if [[ $CONDA_BUILD_CROSS_COMPILATION -eq 1 ]]; then
-#     # Needed?:
-#     # MESON_ARGS="--cross-file ${CONDA_PREFIX}/meson_cross_file.txt"
-#     echo "[properties]" >> ${CONDA_PREFIX}/meson_cross_file.txt
-#     echo "skip_sanity_check = true" >> ${CONDA_PREFIX}/meson_cross_file.txt
-# fi
-
-cat ${CONDA_PREFIX}/meson_cross_file.txt
 
 # meson-python already sets up a -Dbuildtype=release argument to meson, so
 # we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
@@ -34,6 +26,8 @@ $PYTHON -m build -w -n -x \
 
 
 $PYTHON -m pip install dist/pyfmmlib*.whl
+
+# }}}
 
 echo "======================================================" >> README.rst
 echo "fmmlib3d is licensed as follows" >> README.rst

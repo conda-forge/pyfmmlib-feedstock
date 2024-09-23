6c94b5018e44cb9597f2eb81508601a9c442d448
Matthias Diener <mdiener@illinois.edu>
2024-09-23 12:47:04 -0500

    Revert "build directly"
    This reverts commit 76ec8baa0b045450859e1fa647cc0b7ff693d828.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,20 +4,14 @@ set -ex
 
 export LDFLAGS="$LDFLAGS -fopenmp"
 
-mkdir builddir
-
 # Taken from scipy-feedstock
 # HACK: extend $CONDA_PREFIX/meson_cross_file that's created in
 # https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
 # https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
 # to use host python; requires that [binaries] section is last in meson_cross_file
 echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
-cat ${CONDA_PREFIX}/meson_cross_file.txt
 
-
-# -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-$PYTHON -m build -w -n -x \
-    -Cbuilddir=builddir
+pip install . -vv
 
 echo "======================================================" >> README.rst
 echo "fmmlib3d is licensed as follows" >> README.rst
